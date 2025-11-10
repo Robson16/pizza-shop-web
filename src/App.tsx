7cd7314df7ec54134c2e2d5a95be89c1552083d0
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-11-10 16:29:49 -0300

    Setup do projeto - Instalando shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,12 @@
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <h1>Hello Pizza Shop</h1>
+  return (
+    <div className="flex items-center justify-center h-screen min-h-screen">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold mb-8">Hello Pizza Shop</h1>
+        <Button>Enviar</Button>
+      </div>
+    </div>
+  )
 }
