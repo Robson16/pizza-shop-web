1fd86bce44e1376c05daf8617d9fb6b19766b345
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-11-19 17:09:14 -0300

    Páginas e componentes (UI) - ThemeToggle e menu da conta

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { RouterProvider } from 'react-router-dom'
 
 import { Toaster } from '@/components/ui/sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <>
-      <RouterProvider router={router} />
-      <Toaster richColors />
+      <ThemeProvider storageKey="pizza-shop-theme" defaultTheme="dark">
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
     </>
   )
 }
