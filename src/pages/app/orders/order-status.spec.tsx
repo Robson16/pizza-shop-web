d718deb4bc44301735744c1cb0cccada2ec8d45a
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-12-18 16:06:20 -0300

    Testes e mocks - Correção do setup do ambiente de testes

--- src/pages/app/orders/order-status.spec.tsx
+++ src/pages/app/orders/order-status.spec.tsx
@@ -1,7 +1,4 @@
-import '@testing-library/jest-dom'
-
 import { render } from '@testing-library/react'
-import { describe, expect, it } from 'vitest'
 
 import { OrderStatus } from './order-status'
 
