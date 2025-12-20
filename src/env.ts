9a002700d6acc0dca884bae6492d208579f20f32
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-12-20 10:48:55 -0300

    Testes e mocks - Setup do MSW

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,7 @@
 import * as zod from 'zod'
 
 const envSchema = zod.object({
+  MODE: zod.enum(['production', 'development', 'test']),
   VITE_API_URL: zod.url(),
   VITE_ENABLE_API_DELAY: zod.string().transform((value) => value === 'true'),
 })
