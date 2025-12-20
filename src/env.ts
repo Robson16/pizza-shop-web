45d5b101c63eb19cdc3957487d603675c7b1acc3
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-12-20 11:26:50 -0300

    Testes e mocks - Mock de autenticação

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ import * as zod from 'zod'
 
 const envSchema = zod.object({
   MODE: zod.enum(['production', 'development', 'test']),
-  VITE_API_URL: zod.url(),
+  VITE_API_URL: zod.string(),
   VITE_ENABLE_API_DELAY: zod.string().transform((value) => value === 'true'),
 })
 
