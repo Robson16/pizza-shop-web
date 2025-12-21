2ce8af87262c3c1e39aadcf262c3677d79c0a0d0
Robson Henrique Rodrigues <robhenrod@gmail.com>
2025-12-21 20:50:25 -0300

    Testes e mocks - Mock de perfil e atualização

---
+++ src/services/mocks/get-managed-restaurant-mock.ts
@@ -0,0 +1,18 @@
+import { http, HttpResponse } from 'msw'
+
+import type { GetManagedRestaurantResponse } from '../get-managed-restaurant'
+
+export const getManagedRestaurantMock = http.get<
+  never,
+  never,
+  GetManagedRestaurantResponse
+>('/managed-restaurant', () => {
+  return HttpResponse.json({
+    id: 'custom-restaurant-id',
+    managerId: 'custom-user-id',
+    name: 'Pizza Shop',
+    description: '',
+    createdAt: new Date(),
+    updatedAt: null,
+  })
+})
